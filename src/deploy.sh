d2d88a5dbfaaa46060ef9f9e998841e07417cef1
Ruben Vermeersch <ruben@savanne.be>
2011-12-28 21:00:16 +0100

    Progress messages.

--- src/deploy.sh
+++ src/deploy.sh
@@ -7,8 +7,29 @@ if [ "" == "$1" ]; then
     exit;
 fi
 
-git checkout $1
+print_header() {
+    echo -e "\033[1;32m###\033[0m \033[34m$1\033[0m"
+}
+
+print_progress() {
+    echo -e "\033[1;32m### ->\033[0m \033[34m$1\033[0m"
+}
+
+print_header "Deploying to $1"
+
+print_progress "Switching to $1"
+git checkout -q -B $1 origin/$1
+
+print_progress "Pulling latest changes"
 git pull
+
+print_progress "Merging master"
 git merge master
+
+print_progress "Pushing changes upstream"
 git push origin $1
+
+print_progress "Switching back to master"
 git checkout master
+
+print_progress "All done"
